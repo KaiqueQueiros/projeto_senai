ec357d0ea16dd5dd6989adbe569ede3ff5b41c18
Kaique Queiroz <124009154+KaiqueQueiros@users.noreply.github.com>
2023-02-24 16:24:46 -0300

    Configurando o swegger

--- src/main/java/trevo/agro/api/ApiApplication.java
+++ src/main/java/trevo/agro/api/ApiApplication.java
@@ -2,6 +2,8 @@ package trevo.agro.api;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
 
 @SpringBootApplication
 public class ApiApplication {
--- src/main/java/trevo/agro/api/controller/BudgetController.java
+++ src/main/java/trevo/agro/api/controller/BudgetController.java
@@ -2,15 +2,12 @@ package trevo.agro.api.controller;
 
 import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.data.domain.Page;
-import org.springframework.data.domain.Pageable;
-import org.springframework.data.web.PageableDefault;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
-import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.bind.annotation.*;
-import org.springframework.web.util.UriComponentsBuilder;
-import trevo.agro.api.budget.*;
+import trevo.agro.api.budget.BudgetDTO;
+import trevo.agro.api.budget.BudgetRepository;
+import trevo.agro.api.budget.BudgetService;
 import trevo.agro.api.product.ProductRepository;
 import trevo.agro.api.utils.ResponseModel;
 
@@ -24,28 +21,30 @@ public class BudgetController {
     @Autowired
     private BudgetService service;
 
-    @RequestMapping(method = RequestMethod.POST,value = "/register",consumes = MediaType.APPLICATION_JSON_VALUE, produces = MediaType.APPLICATION_JSON_VALUE)
+    @RequestMapping(method = RequestMethod.POST, value = "/register", consumes = MediaType.APPLICATION_JSON_VALUE, produces = MediaType.APPLICATION_JSON_VALUE)
     public ResponseEntity<ResponseModel> register(@RequestBody @Valid BudgetDTO dto) {
         return service.register(dto);
     }
 
-    @RequestMapping(value =  "/list",method = RequestMethod.GET)
-    public ResponseEntity<ResponseModel> list(){
+    @RequestMapping(value = "/list", method = RequestMethod.GET)
+    public ResponseEntity<ResponseModel> list() {
         return service.list();
     }
 
-    @RequestMapping(method = RequestMethod.DELETE,value = "/delete/{id}")
+    @RequestMapping(method = RequestMethod.DELETE, value = "/delete/{id}")
     public ResponseEntity<ResponseModel> delete(@PathVariable Long id) {
-            return service.delete(id);
-    }
-    @RequestMapping(method = RequestMethod.GET,value = "/find/{id}")
-    public ResponseEntity<ResponseModel> detailsClient(@PathVariable Long id) {
-        return service.details(id);
-    }@RequestMapping(method = RequestMethod.PUT,value = "/update/{id}")
-    public ResponseEntity<ResponseModel> update(@PathVariable Long id,@RequestBody BudgetDTO dto){
-        return service.update(dto,id);
+        return service.delete(id);
     }
 
+    @RequestMapping(method = RequestMethod.GET, value = "/find/{id}")
+    public ResponseEntity<ResponseModel> detailsClient(@PathVariable Long id) {
+        return service.details(id);
+    }
+
+    @RequestMapping(method = RequestMethod.PUT, value = "/update/{id}")
+    public ResponseEntity<ResponseModel> update(@PathVariable Long id, @RequestBody BudgetDTO dto) {
+        return service.update(dto, id);
+    }
 
 
 }
